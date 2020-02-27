633ed8830aeea3e478a1fb92a21bc47e57ffc949
Ravi Singh <rksingh@isystango.com>
2020-02-28 02:28:38 +0530

    quick fixes

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,6 +1,14 @@
 class Field < ApplicationRecord
-  FIELD_TYPE = ['Text', 'Textarea', 'Password', 'Email', 'Date', 'File', 'Select', 'Number']
- 
+  FIELD_TYPE = [
+    ['Text', 'string'], 
+    ['Textarea', 'text'], 
+    ['Password', 'password'], 
+    ['Email', 'email'], 
+    ['Date', 'date'], 
+    ['File', 'file'], 
+    ['Select', 'select'], 
+    ['Number', 'number']
+  ]
   belongs_to :dataset
   has_many :field_values
 end
